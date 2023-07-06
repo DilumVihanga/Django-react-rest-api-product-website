e09da831a8a35b5ebbe799faa2ea772e2d459cab
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-07-06 21:55:57 +0530

    data transfered from database to front end

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,8 +88,13 @@ DATABASES = {
     }
 }
 
+
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
